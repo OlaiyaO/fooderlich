a03c39f46a807b824e32c819faaa967476d74ffb
Seun <oniyajoeolaiya@gmail.com>
2023-03-06 22:36:08 +0100

    working on this application too

---
+++ lib/screens/webview_screen.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+
+import '../models/models.dart';
+
+class WebViewScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: FooderlichPages.raywenderlich,
+      key: ValueKey(FooderlichPages.raywenderlich),
+      child: const WebViewScreen(),
+    );
+  }
+
+  const WebViewScreen({Key? key}) : super(key: key);
+
+  @override
+  WebViewScreenState createState() => WebViewScreenState();
+}
+
+class WebViewScreenState extends State<WebViewScreen> {
+  final WebViewController webViewController = WebViewController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (WebViewPlatform.instance is AndroidWebViewPlatform) {
+      final AndroidWebViewController androidController =
+      webViewController.platform as AndroidWebViewController;
+    }
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('raywenderlich.com'),
+      ),
+      body: const WebView(
+        initialUrl: 'https://www.raywenderlich.com/',
+      ),
+    );
+  }
+}
